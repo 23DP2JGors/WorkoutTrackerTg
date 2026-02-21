845545479a9653b421d24629221c075692696132
Yegor Gorshakov <A230095JG@rvt.lv>
2026-02-22 00:15:44 +0200

    initializing laravel + vue

---
+++ resources/js/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="p-4">
+    <h1 class="text-2xl font-bold">Workout Tracker</h1>
+    <p>Hello! Welcome to the Workout Tracker application.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style scoped>
+h1 {
+  color: #1f2937;
+}
+</style>
